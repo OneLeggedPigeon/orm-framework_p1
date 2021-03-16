008dac060f79b92baae245e551100e9e35eb1410
Nate Ford <nathaniel.ford@revature.net>
2021-03-16 13:53:34 -0500

    Added methods to check existing tables

--- src/main/java/com/revature/orm/db/ddl/TableManager.java
+++ src/main/java/com/revature/orm/db/ddl/TableManager.java
@@ -1,25 +1,52 @@
 package com.revature.orm.db.ddl;
 
+import com.revature.orm.ORMLogger;
 import com.revature.orm.config.DBProperties;
 import com.revature.orm.db.dml.read.SQLQueryService;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.HashSet;
 
 public abstract class TableManager {
+
+    // A hashset of the names of all the tables in the current schema, per last read
+    private static final HashSet<String> tableNames = new HashSet<>();
+
+    private static boolean tablesChanged = true;
 
     // Check the following:
     // SELECT table_name FROM INFORMATION_SCHEMA.tables where table_schema like 'p_0';
     public static boolean tableExists(String table){
-        // TODO
-        String schema = DBProperties.getInstance().getPropertyByKey("");
+        return getTableNames().contains(table);
+    }
+
+    // Reads the name of every table into HashSet<String> tableNames
+    private static void readTableNames(){
+        DBProperties prop = DBProperties.getInstance();
+        String schema = prop.getSchema();
         try {
-            SQLQueryService.query("SELECT table_name FROM INFORMATION_SCHEMA.tables WHERE table_schema LIKE '"
-                    +schema
-                    +"'")
-            .getString(table);
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
+            ResultSet rs = SQLQueryService.query("SELECT * FROM INFORMATION_SCHEMA.tables WHERE table_schema LIKE '"+schema+"'");
+            tableNames.clear();
+            while (rs.next()) {
+                tableNames.add(rs.getString("table_name"));
+            }
+            tablesChanged = false;
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        return false;
+    }
+
+    // returns the table names, only reading from the database if there has been an update
+    public static HashSet<String> getTableNames() {
+        if(tablesChanged){
+            readTableNames();
+        }
+        return tableNames;
+    }
+
+    // Call this whenever tables are changed or removed
+    public static void setTablesChanged(boolean tablesChanged) {
+        TableManager.tablesChanged = tablesChanged;
     }
 }
--- src/main/java/com/revature/orm/db/dml/read/SQLQueryService.java
+++ src/main/java/com/revature/orm/db/dml/read/SQLQueryService.java
@@ -1,21 +1,36 @@
 package com.revature.orm.db.dml.read;
 
+import com.revature.orm.ORMLogger;
 import com.revature.orm.db.connection.ConnectionSession;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 
 public abstract class SQLQueryService {
-    public static ResultSet query(String statement){
+
+    /**
+     *
+     * @param statement A SQL <code>String</code> to be turned into a <code>Statement</code>
+     *                  with a new <code>Connection</code> from <code>ConnectionSession</code>
+     * @return a <code>ResultSet</code> object that contains the data produced
+     *         by the given query; never <code>null</code>
+     * @exception SQLException if a database access error occurs,
+     * this method is called on a closed <code>Statement</code>, the given
+     *            SQL statement produces anything other than a single
+     *            <code>ResultSet</code> object, the method is called on a
+     * <code>PreparedStatement</code> or <code>CallableStatement</code>
+     * @throws SQLTimeoutException when the driver has determined that the
+     * timeout value that was specified by the {@code setQueryTimeout}
+     * method has been exceeded and has at least attempted to cancel
+     * the currently running {@code Statement}
+     */
+    public static ResultSet query(String statement) throws SQLException {
         try (ConnectionSession ses = new ConnectionSession()) {
             Connection conn = ses.getActiveConnection();
             Statement stmt = conn.createStatement();
             return stmt.executeQuery(statement);
-        } catch (Exception e) {
-            e.printStackTrace();
+        } catch (SQLException e) {
+            ORMLogger.ormLog.debug("Failed Query: "+statement);
+            throw e;
         }
-
-        return null;
     }
 }
